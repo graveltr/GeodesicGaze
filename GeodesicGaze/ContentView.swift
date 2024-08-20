a5cc89806c7fca7f7157c1e7bce3c2e2172d14e7
Trevor Gravely <graveltr@umich.edu>
2024-08-20 09:09:52 -0500

    Initial Commit

---
+++ GeodesicGaze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeodesicGaze
+//
+//  Created by Trevor Gravely on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeodesicGaze/GeodesicGazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeodesicGazeApp.swift
+//  GeodesicGaze
+//
+//  Created by Trevor Gravely on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct GeodesicGazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
