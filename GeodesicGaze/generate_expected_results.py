ba1e7cc21f35adddc80d6df43fd785a1386bd9b2
Trevor Gravely <graveltr@umich.edu>
2024-09-01 22:08:42 -0500

    Elliptic F was actually correct, python uses m = k * k

--- GeodesicGaze/generate_expected_results.py
+++ GeodesicGaze/generate_expected_results.py
@@ -2,14 +2,29 @@ import numpy as np
 from scipy.special import ellipkinc
 
 def elliptic_integral_f(phi, k):
-    value = ellipkinc(phi, k)
+    value = ellipkinc(phi, k*k)
     error = 0.01
     status = 0
 
     return value, error, status
 
-test_angles = [0.0, 0.5, 1.0]
-test_moduli = [0.0, 0.5, 1.0]
+test_angles = [
+    0.0,            # Lower boundary
+    0.1,            # Small angle
+    0.5,            # Mid-range angle
+    1.0,            # Larger angle
+    1.5,            # Near upper boundary (π/2)
+    np.pi / 4      # π/4 (45 degrees)
+]
+
+test_moduli = [
+    0.0,    # Lower boundary (no elliptic effect)
+    0.1,    # Small modulus
+    0.3,    # Small to mid-range modulus
+    0.5,    # Mid-range modulus
+    0.7,    # Mid to high-range modulus
+    0.9    # High-range modulus
+]
 
 expected_results = [elliptic_integral_f(phi, k) for phi, k in zip(test_angles, test_moduli)]
 
--- GeodesicGazeTests/GeodesicGazeTests.swift
+++ GeodesicGazeTests/GeodesicGazeTests.swift
@@ -53,8 +53,23 @@ final class GeodesicGazeTests: XCTestCase {
     }
     
     func testEllintF() {
-        let testAngles: [Float] = [0.0, 0.5, 1.0]
-        let testModuli: [Float] = [0.0, 0.5, 1.0]
+        let testAngles: [Float] = [
+            0.0,            // Lower boundary
+            0.1,            // Small angle
+            0.5,            // Mid-range angle
+            1.0,            // Larger angle
+            1.5,            // Near upper boundary (π/2)
+            Float.pi / 4   // π/4 (45 degrees)
+        ]
+
+        let testModuli: [Float] = [
+            0.0,    // Lower boundary (no elliptic effect)
+            0.1,    // Small modulus
+            0.3,    // Small to mid-range modulus
+            0.5,    // Mid-range modulus
+            0.7,    // Mid to high-range modulus
+            0.9    // High-range modulus
+        ]
         
         var expectedResults = readExpectedResults()
         
